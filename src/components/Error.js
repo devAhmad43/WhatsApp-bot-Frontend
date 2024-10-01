85c6abb4e7b19f728d0bb95a16dec6186cb6a4d2
devahmad47 <linkplayer423@gmail.com>
2024-10-01 11:59:52 +0500

    1-10-2024

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from "react";
 const Error = () => {
   return (
     <>
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <main className="grid min-h-full place-items-center bg-white px-8 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600">404</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
@@ -19,7 +19,6 @@ const Error = () => {
             >
               Go back home
             </a>
-           
           </div>
         </div>
       </main>
